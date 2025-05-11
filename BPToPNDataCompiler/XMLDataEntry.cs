def3c578a926c4227e4590acd0174ddb2963f62c
halosm1th <16440148+halosm1th@users.noreply.github.com>
2025-05-11 13:24:46 -0500

    Added the XML data entry file to a new file

---
+++ BPToPNDataCompiler/XMLDataEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace DefaultNamespace;
+
+public class XMLDataEntry : BPDataEntry
+{
+    public XMLDataEntry(string fileName) : base(null)
+    {
+        PNFileName = fileName;
+    }
+
+    public string PNFileName { get; set; }
+}
