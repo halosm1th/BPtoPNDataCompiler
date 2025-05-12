4fa859ed9f6dd785b75d606fc6c60790d227a476
halosm1th <16440148+halosm1th@users.noreply.github.com>
2025-05-12 11:03:24 -0500

    I think I rewrote some stuff, tbh not sure.

--- BPToPNDataCompiler/BPDataEntry.cs
+++ BPToPNDataCompiler/BPDataEntry.cs
@@ -4,39 +4,126 @@ namespace DefaultNamespace;
 
 public class BPDataEntry
 {
-    private string? index = null;
+    private string? _bpNum = null;
+    private string? _cr = null;
+    private string? _index = null;
+    private string? _indexBis = null;
+    private string? _internet = null;
+    private string? _name = null;
+    private string? _no = null;
+    private string? _publication = null;
+    private string? _resume = null;
+    private string? _sbandseg = null;
+    private string? _title = null;
+    private string? annee = null;
 
     //At a minimum all entries must have one number
-    public BPDataEntry(string number)
+    public BPDataEntry(string? number)
     {
         BPNumber = number;
     }
 
     public string? Index
     {
-        get { return index; }
-        set { index = ReplaceInvalidText(value); }
+        get => _index;
+        set => _index = ReplaceInvalidText(value);
     }
 
-    public bool HasIndex => index != null;
+    public bool HasIndex => _index != null;
 
-    public string? indexBis { get; set; }
-    public string? title { get; set; }
-    public string? publication { get; set; }
-    public string? internet { get; set; }
-    public string? SBandSEG { get; set; }
-    public string? No { get; set; }
-    public string? annee { get; set; }
-    public string? resume { get; set; }
-    public string? CR { get; set; }
-    public string? Name { get; set; }
-    public string BPNumber { get; set; }
-
-    private string ReplaceInvalidText(string value)
+    public string? IndexBis
     {
-        value = value.Replace("&", "&amp;");
-        value = value.Replace("<", "&lt;");
-        value = value.Replace(">", "&gt;");
+        get => _indexBis;
+        set => _indexBis = ReplaceInvalidText(value);
+    }
+
+    public bool HasIndexBis => _indexBis != null;
+
+    public string? Title
+    {
+        get => _title;
+        set => _title = ReplaceInvalidText(value);
+    }
+
+    public bool HasTitle => _title != null;
+
+    public string? Publication
+    {
+        get => _publication;
+        set => _publication = ReplaceInvalidText(value);
+    }
+
+    public bool HasPublication => _publication != null;
+
+    public string? Internet
+    {
+        get => _internet;
+        set => _internet = ReplaceInvalidText(value);
+    }
+
+    public bool HasInternet => _internet != null;
+
+    public string? SBandSEG
+    {
+        get => _sbandseg;
+        set => _sbandseg = ReplaceInvalidText(value);
+    }
+
+    public bool HasSBandSEG => _sbandseg != null;
+
+    public string? No
+    {
+        get => _no;
+        set => _no = ReplaceInvalidText(value);
+    }
+
+    public bool HasNo => _no != null;
+
+    public string? Annee
+    {
+        get => annee;
+        set => annee = ReplaceInvalidText(value);
+    }
+
+    public bool HasAnnee => annee != null;
+
+    public string? Resume
+    {
+        get => _resume;
+        set => _resume = ReplaceInvalidText(value);
+    }
+
+    public bool HasResume => _resume != null;
+
+    public string? CR
+    {
+        get => _cr;
+        set => _cr = ReplaceInvalidText(value);
+    }
+
+    public bool HasCR => _cr != null;
+
+    public string? Name
+    {
+        get => _name;
+        set => _name = ReplaceInvalidText(value);
+    }
+
+    public bool HasName => _name != null;
+
+    public string? BPNumber
+    {
+        get => _bpNum;
+        set => _bpNum = ReplaceInvalidText(value);
+    }
+
+    public bool HasBPNum => _bpNum != null;
+
+    private string? ReplaceInvalidText(string? value)
+    {
+        value = value?.Replace("&", "&amp;");
+        value = value?.Replace("<", "&lt;");
+        value = value?.Replace(">", "&gt;");
         return value;
     }
 
@@ -44,18 +131,18 @@ public class BPDataEntry
     {
         var sb = new StringBuilder();
         sb.Append($"<idno type=\"bp\">{BPNumber}</idno>");
-        if (index != null) sb.Append($"<seg type=\"original\" subtype=\"index\" resp=\"#BP\">{index}</seg>");
-        if (indexBis != null) sb.Append($"<seg type=\"original\" subtype=\"indexBis\" resp=\"#BP\">{indexBis}</seg>");
-        if (title != null) sb.Append($"<seg type=\"original\" subtype=\"titre\" resp=\"#BP\">{title}</seg>");
+        if (_index != null) sb.Append($"<seg type=\"original\" subtype=\"index\" resp=\"#BP\">{_index}</seg>");
+        if (IndexBis != null) sb.Append($"<seg type=\"original\" subtype=\"indexBis\" resp=\"#BP\">{IndexBis}</seg>");
+        if (_title != null) sb.Append($"<seg type=\"original\" subtype=\"titre\" resp=\"#BP\">{_title}</seg>");
         if (SBandSEG != null) sb.Append($"<seg type=\"original\" subtype=\"SBandSeg\" resp=\"#BP\">{SBandSEG}</seg>");
         if (No != null) sb.Append($"<seg type=\"original\" subtype=\"No\" resp=\"#BP\">{No}</seg>");
         if (annee != null) sb.Append($"<seg type=\"original\" subtype=\"annee\" resp=\"#BP\">{annee}</seg>");
-        if (publication != null)
-            sb.Append($"<seg type=\"original\" subtype=\"publication\" resp=\"#BP\">{publication}</seg>");
-        if (resume != null) sb.Append($"<seg type=\"original\" subtype=\"resume\" resp=\"#BP\">{resume}</seg>");
+        if (Publication != null)
+            sb.Append($"<seg type=\"original\" subtype=\"publication\" resp=\"#BP\">{Publication}</seg>");
+        if (_resume != null) sb.Append($"<seg type=\"original\" subtype=\"resume\" resp=\"#BP\">{_resume}</seg>");
         if (CR != null) sb.Append($"<seg type=\"original\" subtype=\"cr\" resp=\"#BP\">{CR}</seg>");
         if (Name != null) sb.Append($"<seg type=\"original\" subtype=\"nom\" resp=\"#BP\">{Name}</seg>");
-        if (internet != null) sb.Append($"<ptr target=\"{internet}\"");
+        if (_internet != null) sb.Append($"<ptr target=\"{_internet}\"");
 
         return sb.ToString();
     }
